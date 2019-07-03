82c3e90fec2d664c5017a12ba4704d1395235ed0
KKGao <gaojinkai@qiangungun.com>
2019-07-03 13:36:34 +0800

    添加底部导航图片

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,10 @@
 
 class AppColors {
   static const AppBarColor = 0xff303030;
+  static const TabNormalColor = 0xff303030;
+  static const TabActiveColor = 0xff303030;
+}
+
+class Constants {
+  static const String IconFontFamily = 'appIconFont';
 }
